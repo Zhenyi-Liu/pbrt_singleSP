4ebc8f1b01055ea7041e2c45a379d38c18705a9d
Zhenyi-Liu <zhenyiliu@stanford.edu>
2018-12-03 15:48:47 -0800

    Modified spectrum samples to be 400:10:1000 nm

--- src/core/spectrum.h
+++ src/core/spectrum.h
@@ -46,8 +46,8 @@ namespace pbrt {
 
 // Spectrum Utility Declarations
 static const int sampledLambdaStart = 395;
-static const int sampledLambdaEnd = 705;
-static const int nSpectralSamples = 31;
+static const int sampledLambdaEnd = 1005;
+static const int nSpectralSamples = 61;
 extern bool SpectrumSamplesSorted(const Float *lambda, const Float *vals,
                                   int n);
 extern void SortSpectrumSamples(Float *lambda, Float *vals, int n);
