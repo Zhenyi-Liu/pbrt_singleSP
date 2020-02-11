a6c9eedbc774e0abc2c6fe6672554028c60d1bb1
Zhenyi Liu <zhenyi27@holidayfun.c.primal-surfer-140120.internal>
2020-02-10 22:09:40 -0800

    440 nm

--- src/core/spectrum.h
+++ src/core/spectrum.h
@@ -48,8 +48,8 @@ namespace pbrt {
 //static const int sampledLambdaStart = 395;
 //static const int sampledLambdaEnd = 705;
 //static const int nSpectralSamples = 31;
-static const int sampledLambdaStart = 425;
-static const int sampledLambdaEnd = 435;
+static const int sampledLambdaStart = 435;
+static const int sampledLambdaEnd = 445;
 static const int nSpectralSamples = 1;
 extern bool SpectrumSamplesSorted(const Float *lambda, const Float *vals,
                                   int n);
